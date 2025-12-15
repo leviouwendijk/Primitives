a5d53293125949560d5bc9dad4d6c1b43e1a200a
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-15 21:16:12 +0100

    init

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Primitives",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Primitives",
+            targets: ["Primitives"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Primitives"
+        ),
+        .testTarget(
+            name: "PrimitivesTests",
+            dependencies: ["Primitives"]
+        ),
+    ]
+)
