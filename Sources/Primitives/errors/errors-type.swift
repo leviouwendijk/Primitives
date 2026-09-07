99df450665a2a6b4d9f91cda59b6bcc17e0b2d6f
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2026-09-07 09:44:58 +0200

    deprecate -> Errors package

--- Sources/Primitives/errors/errors-type.swift
+++ Sources/Primitives/errors/errors-type.swift
@@ -1,11 +1,21 @@
-import Foundation
-
-public typealias MultiError = Errors
-
+@available(
+    *,
+    deprecated,
+    message: "Import Errors and use Errors from the Errors package."
+)
 public struct Errors: Error, Sendable {
-    public let errors: [Error]
+    public let errors: [any Error]
 
-    public init(_ errors: [Error]) {
+    public init(
+        _ errors: [any Error]
+    ) {
         self.errors = errors
     }
 }
+
+@available(
+    *,
+    deprecated,
+    message: "Import Errors and use Errors from the Errors package."
+)
+public typealias MultiError = Errors
